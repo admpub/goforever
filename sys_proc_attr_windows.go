f1adb77f189f9e79f4a691cd19168b6b87b59df9
Hank Shen <swh@admpub.com>
2023-07-23 18:21:05 +0800

    update

--- sys_proc_attr_windows.go
+++ sys_proc_attr_windows.go
@@ -3,10 +3,34 @@
 package goforever
 
 import (
-	"golang.org/x/sys/windows"
+	"fmt"
+	"golang.org/sys/windows"
 	"syscall"
 )
 
 func (p *Process) setSysProcAttr(attr *syscall.SysProcAttr) error {
-	attr.Token = windows.Token(0)
+	windows.LookupSID(``)
+	token, err := getToken(0)
+	if err != nil {
+		return err
+	}
+	attr.Token = token
+	return nil
+}
+
+func getToken(pid int) (syscall.Token, error) {
+	var token syscall.Token
+
+	handle, err := syscall.OpenProcess(syscall.PROCESS_QUERY_INFORMATION, false, uint32(pid))
+	if err != nil {
+		return token, fmt.Errorf("Token Process Error: %w", err)
+	}
+	defer syscall.CloseHandle(handle)
+
+	// Find process token via win32
+	err = syscall.OpenProcessToken(handle, syscall.TOKEN_ALL_ACCESS, &token)
+	if err != nil {
+		return token, fmt.Errorf("Open Token Process Error: %w", err)
+	}
+	return token, err
 }
